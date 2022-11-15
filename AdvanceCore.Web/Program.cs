3d48061c9cde20b72463085211552bb817eac1e5
khabzcodes <khabubundivhuwo@gmail.com>
2022-11-15 19:34:02 +0200

    add blazor blazor assembly project setup

--- AdvanceCore.Web/Program.cs
+++ AdvanceCore.Web/Program.cs
@@ -1,4 +1,6 @@
 using AdvanceCore.Web;
+using AdvanceCore.Web.Services;
+using AdvanceCore.Web.Services.Implementation;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7061") });
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 await builder.Build().RunAsync();
